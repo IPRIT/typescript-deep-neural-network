2b6f099e214078f2b932aeb9ab2b764b69646a0b
ipritoflex <ipritoflex@yandex.ru>
2016-03-30 20:48:22 +0300

    Update

--- Neuro/NeuralLearning/BackPropagationLearning.ts
+++ Neuro/NeuralLearning/BackPropagationLearning.ts
@@ -4,12 +4,26 @@ import {Network} from "../Network/Network";
 export class BackPropagationLearning implements INeuralLearningStrategy {
 
   type: LearningMethod = LearningMethod.BackPropagation;
+  private neuronErrors: number[][] = [[]];
+  private weightUpdates: number[][][] = [[[]]];
+  private boundUpdates: number[][] = [[]];
 
   constructor(public network: Network, public n: number) {
+    
+    for (let i = 0; i < network.length; ++i) {
+      for (let j = 0; j < network.layers[i].length; ++j) {
 
+      }
+    }
   }
 
   learn(input: number[][], output: number[][]) {
     
+  }
+
+  private getFilledArray(length: number, value: any) {
+    return Array
+        .apply(null, Array(length))
+        .map(Number.prototype.valueOf, value);
   }
 }
