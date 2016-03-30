193f60d7f90d43964ddd64b0366209b96a3e5331
ipritoflex <ipritoflex@yandex.ru>
2016-03-30 19:15:04 +0300

    Update

---
+++ Neuro/Data/DataNormalizer.ts
@@ -0,0 +1,13 @@
+interface INormalizable {
+
+    normalize(input: number[][], intervalFrom?: number, intervalTo?: number): number[][];
+}
+
+export class DataNormalizer implements INormalizable {
+
+    normalize(input, from = 0, to = 1) {
+
+
+        return <any>{};
+    }
+}
